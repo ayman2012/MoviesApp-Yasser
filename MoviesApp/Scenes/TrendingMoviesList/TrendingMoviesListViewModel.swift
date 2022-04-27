1bdb4c1068056ad4bb1827d899f1b673d91af460
Ayman Fathy <aymanfathy367@gmail.com>
2022-04-27 05:09:18 +0200

    Restruct project files

---
+++ MoviesApp/Scenes/TrendingMoviesList/TrendingMoviesListViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  TrendingMoviesListViewModel.swift
+//  MoviesApp
+//
+//  Created by Ayman Fathy on 27/04/2022.
+//
+
+import Foundation
+
+protocol TrendingMoviesListViewModelProtocol {
+    func getTrendingMoviesList() -> Void
+}
+
+class TrendingMoviesListViewModel: TrendingMoviesListViewModelProtocol {
+    
+    func getTrendingMoviesList() {
+        
+    }
+    
+}
