20326f1add21c5dce5b41f72ce7bdafc53c078ba
Ayman Fathy <aymanfathy367@gmail.com>
2022-04-27 04:59:54 +0200

    Setup project files

---
+++ MoviesApp/Scenes/TrendingMoviesList/TrendingMoviesListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TrendingMoviesListViewController.swift
+//  MoviesApp
+//
+//  Created by Ayman Fathy on 27/04/2022.
+//
+
+import UIKit
+
+class TrendingMoviesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
